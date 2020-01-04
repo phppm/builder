9ba1cf4d3c9db7ba7a96b17b95e992aeae985757
阳光锈了 <rusty.sun@qq.com>
2020-01-04 08:59:19 +0800

    fixed:版本

--- resource/plan/rustphp/rust/landsker/template/ErrCode.php
+++
@@ -1,4 +0,0 @@
-//<?php echo $model->getClassNote(),"\n";?>
-const <?php echo strtoupper($model->getTableModel()->getName());?>_GET_FAILED = <?php echo($model->getProjectErrorNum()+201);?>;
-const <?php echo strtoupper($model->getTableModel()->getName());?>_REMOVE_FAILED = <?php echo($model->getProjectErrorNum()+202);?>;
-const <?php echo strtoupper($model->getTableModel()->getName());?>_SAVE_FAILED=<?php echo($model->getProjectErrorNum()+203);?>;
